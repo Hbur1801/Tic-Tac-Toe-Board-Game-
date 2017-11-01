19572866b49aff5fef5bc85b359ce5e930fbba49
BURTON HOWARD A <hbur1801@education.mpls.k12.mn.us>
2017-11-01 09:59:12 -0500

    The last one mannn

--- Tic tac toe game.py
+++ Tic tac toe game.py
@@ -1,16 +1,34 @@
 # Tic tac toe Boi 
-import Tkinter
+from Tkinter import *   # don't pollute namespace like this except for Tkinter
+import PIL    
+from PIL import ImageTk # a subpackage that must be imported explicitly
 
-screen = Tkinter.Tk()
+import os.path              
+__dir__ = os.path.dirname(os.path.abspath(__file__))  
+filename = os.path.join(__dir__, 'tictacx.png')
+img = PIL.Image.open(filename)
+
+
+
+
+screen = Tk()
 screen.title("Tic Tac Toe Boi")
 
 #Create a board
-board = Tkinter.Canvas(screen,width=300,height=300)
+board = Canvas(screen,width=300,height=300)
 board.pack()
 
 
 board.create_rectangle(0,0,300,300, outline="black")
 board.create_rectangle(100,300,200,0, outline="black")
 board.create_rectangle(0,100,300,200, outline="black")
+tkimg=PIL.ImageTk.PhotoImage(img)
+def stamp(event):
+    board.create_image(event.x,event.y,image=tkimg)
+    board.create_rectangle(40,50,300,200, outline="red")
+# Bind event to handler
+
+board.bind('<ButtonPress-1>', stamp)
+
 
 screen.mainloop()
